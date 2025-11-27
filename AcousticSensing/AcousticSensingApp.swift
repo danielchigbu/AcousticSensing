d3fa87b71546ea2c043adc4d05adad4999d44852
Daniel Chigbu <ebubechigbu2001@gmail.com>
2025-11-27 01:17:18 -0500

    Initial Commit

---
+++ AcousticSensing/AcousticSensingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcousticSensingApp.swift
+//  AcousticSensing
+//
+//  Created by Daniel Chigbu on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AcousticSensingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
